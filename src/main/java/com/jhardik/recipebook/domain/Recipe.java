777f5d868dea01e93c4e91103eccf9ad701ffad5
Hardik Joshi <joshi.hardik654@gmail.com>
2019-09-20 01:23:20 +0530

    refactor some code to remove unwanted recipe adding in bootstrap so we directly put into POJOs

--- src/main/java/com/jhardik/recipebook/domain/Recipe.java
+++ src/main/java/com/jhardik/recipebook/domain/Recipe.java
@@ -116,6 +116,12 @@ public class Recipe {
         this.difficulty = difficulty;
     }
 
+    public Recipe addIngredient(Ingredient ingredient) {
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
+    }
+
     public Set<Ingredient> getIngredients() {
         return ingredients;
     }
@@ -130,6 +136,7 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        this.notes.setRecipe(this);
     }
 
     public Set<Category> getCategories() {
