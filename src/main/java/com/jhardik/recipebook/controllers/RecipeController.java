7c4be00d3cab9ccb63942b750ce522c3973b08dd
Hardik Joshi <joshi.hardik654@gmail.com>
2019-10-21 21:17:57 +0530

    Added 404 Error Page when Recipe not found

--- src/main/java/com/jhardik/recipebook/controllers/RecipeController.java
+++ src/main/java/com/jhardik/recipebook/controllers/RecipeController.java
@@ -1,14 +1,14 @@
 package com.jhardik.recipebook.controllers;
 
 import com.jhardik.recipebook.commands.RecipeCommand;
+import com.jhardik.recipebook.exceptions.NotFoundException;
 import com.jhardik.recipebook.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -53,5 +53,17 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound() {
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404-error");
+
+        return modelAndView;
     }
 }
